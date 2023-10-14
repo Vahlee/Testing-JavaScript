14d1fb0a4d4e6f761430c1b00a30406f4ab1e827
Violet Ovedje <132350410+Vahlee@users.noreply.github.com>
2023-10-14 22:42:24 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        var myH1 = document.getElementById("myH1");
+
+        myH1.innerHTML = "Haaland 🍆";
+    }, 2000);
+});
